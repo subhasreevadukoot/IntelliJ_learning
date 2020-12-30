4b4fcacf7ba0a73f7a3f52bed2e3186cf35a69df
Subhasree <subhasree.vadukoot@student.griffith.ie>
2020-12-30 10:33:46 +0000

    Initial commit

---
+++ Generate/src/com/example/idea/Main.java
@@ -0,0 +1,14 @@
+package com.example.idea;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello from IntelliJ IDEA!");
+
+        //noinspection unused
+        String firstname;
+        int num;
+
+    }
+}
